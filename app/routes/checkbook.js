01d6f6bd77c4ae3d73685c2ac2180f22dd2aee15
tae yoon <taenaive@gmail.com>
2016-11-02 08:36:55 -0400

    added micr font

--- app/routes/checkbook.js
+++ app/routes/checkbook.js
@@ -4,11 +4,10 @@ export default Ember.Route.extend({
   setupController(controller, model) {
 
     this._super(controller, model);
-    let date = new Date()
-    let datePortion = Math.trunc(date.getDate()) < 10 ? `0${date.getDate()}` : date.getDate()
-    Ember.Logger.log('today:', date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + datePortion)
-    //controller.set('dateIn', date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate());
-    controller.set('dateIn',date);
-  },
+    let date = new Date();
+    let datePortion = Math.trunc(date.getDate()) < 10 ? `0${date.getDate()}` : date.getDate();
+    Ember.Logger.log('today:', date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + datePortion);
+    controller.set('dateIn', date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + datePortion);
+    //controller.set('dateIn',date);
   },
 });
